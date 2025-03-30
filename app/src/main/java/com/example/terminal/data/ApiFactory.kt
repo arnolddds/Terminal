131c9ee5a70155cb45ac20bff81b2a2a3d60bd93
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-03-30 20:04:59 +0700

    Добавление okHttpClient

--- app/src/main/java/com/example/terminal/data/ApiFactory.kt
+++ app/src/main/java/com/example/terminal/data/ApiFactory.kt
@@ -1,5 +1,7 @@
 package com.example.terminal.data
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -8,9 +10,16 @@ object ApiFactory {
 
     private const val BASE_URL = "https://api.polygon.io/v2/"
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
         .build()
 
     val apiService: ApiService = retrofit.create()
