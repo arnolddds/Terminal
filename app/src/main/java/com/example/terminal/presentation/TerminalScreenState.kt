5f58ad0514df43b0f1ef3517dedd77931a586bf1
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-03-30 14:53:31 +0700

    Добавление вью модели

---
+++ app/src/main/java/com/example/terminal/presentation/TerminalScreenState.kt
@@ -0,0 +1,11 @@
+package com.example.terminal.presentation
+
+import com.example.terminal.data.Bar
+
+sealed class TerminalScreenState {
+
+    data object Initial: TerminalScreenState()
+
+    data class Content(val barList: List<Bar>): TerminalScreenState()
+
+}
