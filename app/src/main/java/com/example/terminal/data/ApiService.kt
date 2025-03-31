f53744bf18a948fabc6dd71b7fba8c3bafd4fb51
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-03-31 14:03:19 +0700

    Масштабирование свечей

--- app/src/main/java/com/example/terminal/data/ApiService.kt
+++ app/src/main/java/com/example/terminal/data/ApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 interface ApiService {
 
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2024-01-09?adjusted=true&sort=asc&limit=50000&apiKey=RPxwcp8QQseffWL_OjvcYzwpfJ__o0GI")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2024-01-09?adjusted=true&sort=desc&limit=50000&apiKey=RPxwcp8QQseffWL_OjvcYzwpfJ__o0GI")
     suspend fun loadBars(): Result
 }
