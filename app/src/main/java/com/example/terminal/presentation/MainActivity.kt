050bd6ddd6ff063539090c19f2531f673f482700
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-03-31 13:40:48 +0700

    Начальный график свечей

--- app/src/main/java/com/example/terminal/presentation/MainActivity.kt
+++ app/src/main/java/com/example/terminal/presentation/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : ComponentActivity() {
                             bars = currentState.barList
                         )
                     }
-
                     is TerminalScreenState.Initial -> {
 
                     }
--- app/src/main/java/com/example/terminal/presentation/Terminal.kt
+++ app/src/main/java/com/example/terminal/presentation/Terminal.kt
@@ -18,13 +18,16 @@ fun Terminal(bars: List<Bar>) {
             .background(Color.Black)
 
     ) {
+        val max = bars.maxOf { it.high }
+        val min = bars.minOf { it.low }
         val barWidth = size.width / bars.size
+        val pxPerPoint = size.height / (max - min)
         bars.forEachIndexed { index, bar ->
             val offsetX = index * barWidth
             drawLine(
                 color = Color.White,
-                start = Offset(offsetX, size.height - bar.low),
-                end = Offset(offsetX, size.height - bar.high),
+                start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
+                end = Offset(offsetX, size.height - ((bar.high - min) * pxPerPoint)),
                 strokeWidth = 1f
             )
         }
