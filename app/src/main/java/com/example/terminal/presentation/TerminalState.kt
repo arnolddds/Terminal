22bc256a99dbafd479611a543a429661db2d1bd9
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-03-31 15:54:39 +0700

    Для переворота экрана парсим стейт

--- app/src/main/java/com/example/terminal/presentation/TerminalState.kt
+++ app/src/main/java/com/example/terminal/presentation/TerminalState.kt
@@ -1,14 +1,18 @@
 package com.example.terminal.presentation
 
+import android.os.Parcelable
 import com.example.terminal.data.Bar
+import kotlinx.parcelize.Parcelize
 import kotlin.math.roundToInt
 
+
+@Parcelize
 data class TerminalState(
     val barList: List<Bar>,
     val visibleBarsCount: Int = 100,
     val terminalWidth: Float = 0f,
     val scrolledBy: Float = 0f
-) {
+): Parcelable {
 
     val barWidth: Float
         get() = terminalWidth / visibleBarsCount
